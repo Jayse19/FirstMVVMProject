208890f47093b3d3ff677e7e34f6991338264e6e
Jayse Baruch <jayse.baruch@students.snow.edu>
2022-01-17 23:00:58 -0700

    Add project files.

---
+++ FirstMVVMProjDice/FirstMVVMProjDice/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FirstMVVMProjDice.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FirstMVVMProjDice.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
